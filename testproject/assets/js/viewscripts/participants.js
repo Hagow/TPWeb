4cd34ae7959a6ab7360e2bf785bf57cc4b31af6b
Christopher Saboya <christopher.saboya@iutnice.unice.fr>
2017-03-16 12:21:31 +0100

    maj ajax (organisation)

---
+++ testproject/assets/js/viewscripts/participants.js
@@ -0,0 +1,14 @@
+ var toto = "salut";
+
+  //alert('hello');
+
+  function test(argv1){
+    alert (argv1);
+
+    var je suisdansuntest = "ben oui"
+  }
+
+  var calcul = fonction(){
+    alert('calcul');
+  }
+    test('ca marche');
